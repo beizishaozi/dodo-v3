51d039543690566c4cfc3cf00ae857ad59aef279
zhangy <984363019@qq.com>
2024-08-20 18:05:45 +0800

    Update D3UserQuota.sol

--- contracts/DODOV3MM/D3Vault/periphery/D3UserQuota.sol
+++ contracts/DODOV3MM/D3Vault/periphery/D3UserQuota.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.16;
+pragma solidity 0.8.20;
 
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "../../../intf/IERC20.sol";
