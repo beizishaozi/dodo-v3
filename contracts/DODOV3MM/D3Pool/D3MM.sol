eda68d1473a58d17dd767c5027ca88f3e9eb15e2
zhangy <984363019@qq.com>
2024-08-20 18:05:10 +0800

    Update D3MM.sol

--- contracts/DODOV3MM/D3Pool/D3MM.sol
+++ contracts/DODOV3MM/D3Pool/D3MM.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: BUSL-1.1
-pragma solidity 0.8.16;
+pragma solidity 0.8.20;
 
 import {D3Trading} from "./D3Trading.sol";
 import {IFeeRateModel} from "../../intf/IFeeRateModel.sol";
