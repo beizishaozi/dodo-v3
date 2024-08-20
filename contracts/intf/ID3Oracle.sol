7a526241a76ff7c8f2bd17f11ec3f882fc5f0010
zhangy <984363019@qq.com>
2024-08-20 18:07:05 +0800

    Update ID3Oracle.sol

--- contracts/intf/ID3Oracle.sol
+++ contracts/intf/ID3Oracle.sol
@@ -5,7 +5,7 @@
 
 */
 
-pragma solidity 0.8.16;
+pragma solidity 0.8.20;
 
 interface ID3Oracle {
     function getMaxReceive(address fromToken, address toToken, uint256 fromAmount) external view returns (uint256);
