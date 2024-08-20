748bb0f170119b19dc43e144d9f6bf9354ea98e5
zhangy <984363019@qq.com>
2024-08-20 15:07:51 +0800

    Update D3VaultStorage.sol

--- contracts/DODOV3MM/D3Vault/D3VaultStorage.sol
+++ contracts/DODOV3MM/D3Vault/D3VaultStorage.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity 0.8.16;
 
-import {ReentrancyGuard} from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
